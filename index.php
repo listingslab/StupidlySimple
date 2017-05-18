95675acbf928044ba72116969299456ad4ccc624
Chris Dorward <listingslab@gmail.com>
2017-05-18 14:31:12 +0930

    Stupidly Simple PHP index file

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Stupidly Simple</title>
+        <style>
+            header {
+                text-align: center;
+            }
+        </style>
+    </head>
+
+    <body>
+        <header>
+            <h1>Stupidly Simple</h1>
+            <p>local dev environment</p>
+        </header>
+
+        <?php
+            phpinfo();
+        ?>
+    </body>
+
+</html>
